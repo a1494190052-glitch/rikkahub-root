ec8b2c0f2e176dc200c7db7250fcfbe3dc8d9ba2
a1494190052-glitch <a1494190052-glitch@users.noreply.github.com>
2026-07-26 21:39:07 +0000

    fix: crashlytics + theme strings

--- app/src/main/java/me/rerere/rikkahub/di/AppModule.kt
+++ app/src/main/java/me/rerere/rikkahub/di/AppModule.kt
@@ -1,8 +1,8 @@
 package me.rerere.rikkahub.di
 
-import com.google.firebase.Firebase
+// import com.google.firebase.Firebase
 import com.google.firebase.analytics.analytics
-import com.google.firebase.crashlytics.crashlytics
+// import com.google.firebase.crashlytics.crashlytics
 import kotlinx.serialization.json.Json
 import me.rerere.highlight.Highlighter
 import me.rerere.rikkahub.AppScope
@@ -118,11 +118,11 @@ val appModule = module {
     }
 
     single {
-        Firebase.crashlytics
+//         Firebase.crashlytics
     }
 
     single {
-        Firebase.analytics
+//         Firebase.analytics
     }
 
     single {
